82c2bec80e66ee362ad42188d2612ef725947dab
Dusan <dusan.jovanovicno.4@gmail.com>
2024-01-16 01:38:55 +0100

    create user controller, services, route and setup

--- src/appRouter.ts
+++ src/appRouter.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import quizzesRouter from "./app/quizzes/api/route";
 import collectorRouter from "./app/collectors/api/route";
+import userRouter from "./app/users/api/route";
 
 const appRouter = express.Router();
 
 appRouter.use("/quiz", quizzesRouter);
 appRouter.use("/collector", collectorRouter);
+appRouter.use("/user", userRouter);
 
 export default appRouter;
