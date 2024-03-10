c78273c2eb12cc9cad0ffd01f96ab0cbcf30ba5c
Dusan <dusan.jovanovicno.4@gmail.com>
2024-03-11 00:44:06 +0100

    add simple rate limiting for api endpoints

--- src/lib/middlewares/index.ts
+++ src/lib/middlewares/index.ts
@@ -1,7 +1,16 @@
 import { Request, Response, NextFunction } from "express";
+import { rateLimit } from "express-rate-limit";
 
 export const asyncHandler =
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (fn: any) => (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch(next);
   };
+
+export const rateLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100,
+  message: "Too many requests! Please try again in a bit.",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,7 @@ import { Server } from "http";
 import "./global-express";
 import config from "./config";
 import helmet from "helmet";
+import { rateLimiter } from "./lib/middlewares";
 
 let connection: Server;
 async function startWebServer(): Promise<AddressInfo> {
@@ -31,10 +32,10 @@ async function startWebServer(): Promise<AddressInfo> {
     })
   );
   app.use(helmet());
-  app.use(express.json());
+  app.use(express.json({ limit: "300kb" }));
   app.use(express.static(path.join(__dirname, "public")));
 
-  app.use(appRouter);
+  app.use(appRouter, rateLimiter);
 
   app.post(
     "/webhook",
